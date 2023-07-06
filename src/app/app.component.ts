87af5ae00d3cee644f73ffc94e2e5354923257eb
Seren AKYÜZ <serenwq@gmail.com>
2023-07-06 12:59:44 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MechanicaIrev';
+  productsData: any;
+  url: string = '/data/products.json';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 }
